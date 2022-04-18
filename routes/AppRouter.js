fcdbd998fb706326b1a71d4fe194877866abf42e
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-04-18 12:08:18 -0400

    Created JWT authentication

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,9 +2,11 @@ const Router = require('express').Router()
 const ReviewRouter = require('./ReviewRouter')
 const CarRouter = require('./CarRouter')
 const PartRouter = require('./PartRouter')
+const AuthRouter = require('./AuthRouter')
 
 Router.use('/reviews', ReviewRouter)
 Router.use('/cars', CarRouter)
 Router.use('/parts', PartRouter)
+Router.use('/auth', AuthRouter)
 
 module.exports = Router
--- routes/PartRouter.js
+++ routes/PartRouter.js
@@ -1,8 +1,15 @@
 const Router = require('express').Router()
 const controller = require('../controllers/PartController')
+const middleware = require('../middleware')
 
-Router.get('/view', controller.GetParts)
-Router.get('/view/:part_id', controller.GetPartDetails)
+Router.get(
+    '/view', 
+    controller.GetParts
+)
+Router.get(
+    '/view/:part_id',
+    controller.GetPartDetails
+)
 
 
 
