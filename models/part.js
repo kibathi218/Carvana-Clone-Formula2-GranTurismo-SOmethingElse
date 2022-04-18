d442bbe036d21dae54e6d5d6583ef236fd33d56f
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-04-18 10:45:27 -0400

    fixed part model error

--- models/part.js
+++ models/part.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Part.belongsTo(models.Car, {foreignKey: 'carId'})
-      Part.belongsTo(models.User, { foreignKey: 'ownerId '})
+      Part.belongsTo(models.User, { foreignKey: 'ownerId'})
     }
   }
   Part.init({
