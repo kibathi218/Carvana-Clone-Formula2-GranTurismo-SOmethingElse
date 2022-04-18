32f00c3f5817279f11bab37a041e63c61677af35
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-04-18 15:36:46 -0400

    fixed authentication user creation errors

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -26,10 +26,30 @@ const Login = async (req, res) => {
 
 const Register = async (req, res) => {
   try {
-    const { email, password, name } = req.body
+    const { email, password, firstName, lastName, username } = req.body
     let passwordDigest = await middleware.hashPassword(password)
-    const user = await User.create({ email, passwordDigest, name })
+    const user = await User.create({ email, passwordDigest, firstName, lastName, username })
     res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+const UpdatePassword = async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } })
+    if (
+      user &&
+      (await middleware.comparePassword(
+        user.dataValues.passwordDigest,
+        req.body.oldPassword
+      ))
+    ) {
+      let passwordDigest = await middleware.hashPassword(req.body.newPassword)
+
+      await user.update({ passwordDigest })
+      return res.send({ status: 'Success', msg: 'Password Updated' })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Invalid Credentials' })
   } catch (error) {
     throw error
   }
@@ -37,5 +57,6 @@ const Register = async (req, res) => {
 
 module.exports = {
   Login,
-  Register
+  Register,
+  UpdatePassword
 }
